e113570b097aed720dedec2a0b63e4ce537befff
Jees1 <49425409+Jees1@users.noreply.github.com>
2019-11-18 21:57:09 +0200

    Added all ranks and ids of ranks

--- drizzleranks/drizzleranks.py
+++ drizzleranks/drizzleranks.py
@@ -22,16 +22,25 @@ class drizzleRanks(commands.Cog):
             title="Drizzle Hotel Ranks"
         )
         embed.description = """
-                > Chairperson
-                > Vice-Chairperson
-                > Executive
-                > Public Relations Officer
-                > Board of Directors
-                > Shift Manager
-                > Supervisor
-                > Security
-                > Receptionist
-                > Trainee
+                > - | Owner __255__
+                > - | Co-Owner __254__
+                > - | Development Team __253__
+                > - | Group Managers __252__
+                > - | Overseers __251__
+                > Chairperson __250__
+                > Vice-Chairperson __13__
+                > Executive __12__
+                > Public Relations Officer __11__
+                > Board of Directors __10__
+                > Shift Manager __9__
+                > Supervisor __8__
+                > Security __7__
+                > Receptionist __6__
+                > Trainee __5__
+                > Allied Represantive __4__
+                > - | Noted Guest __3__
+                > X | Suspended __2__
+                > Hotel Guest __1__
             """
         embed.color = self.bot.main_color
         return await ctx.send(embed=embed)
